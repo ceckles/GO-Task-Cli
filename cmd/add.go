aee58630a018ba1bd4b4c3e560879ca30c08af79
Chad Eckles <chad1020@gmail.com>
2024-08-21 16:24:06 -0500

    added cmd and function to add task to list

--- cmd/add.go
+++ cmd/add.go
@@ -1,40 +1,91 @@
-/*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
-*/
 package cmd
 
 import (
+	"encoding/csv"
 	"fmt"
+	"os"
+	"strconv"
+	"time"
 
+	"github.com/ceckles/GO-Task-Cli/utils"
 	"github.com/spf13/cobra"
 )
 
-// addCmd represents the add command
-var addCmd = &cobra.Command{
-	Use:   "add",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+var csvFilePath = "tasks.csv" // Path to your CSV file
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+var addCmd = &cobra.Command{
+	Use:   "add [task details]",
+	Short: "Add a new task",
+	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("add called")
+		taskDetails := args[0]
+
+		// Open and lock the CSV file using utils.LoadFile
+		file, err := utils.LoadFile(csvFilePath)
+		if err != nil {
+			fmt.Println("Error opening CSV file:", err)
+			return
+		}
+		defer func() {
+			if err := utils.CloseFile(file); err != nil {
+				fmt.Println("Error closing CSV file:", err)
+			}
+		}()
+
+		// Get the next task ID
+		nextID, err := getNextID(file)
+		if err != nil {
+			fmt.Println("Error generating task ID:", err)
+			return
+		}
+
+		// Get the current timestamp
+		timestamp := time.Now().Format("2006-01-02 15:04:05.999999999 -0700 MST")
+
+		// Write the new task to the CSV file
+		writer := csv.NewWriter(file)
+		err = writer.Write([]string{
+			strconv.Itoa(nextID),
+			taskDetails,
+			timestamp,
+			"false",
+		})
+		if err != nil {
+			fmt.Println("Error writing to CSV file:", err)
+			return
+		}
+		writer.Flush()
+
+		fmt.Printf("Task added: %s\n", taskDetails)
 	},
+}
+
+// Modified getNextID to take *os.File as a parameter
+func getNextID(file *os.File) (int, error) {
+	// Reset file pointer to the beginning
+	if _, err := file.Seek(0, 0); err != nil {
+		return 0, err
+	}
+
+	reader := csv.NewReader(file)
+	records, err := reader.ReadAll()
+	if err != nil {
+		return 0, err
+	}
+
+	if len(records) == 0 {
+		return 1, nil
+	}
+
+	lastRecord := records[len(records)-1]
+	lastID, err := strconv.Atoi(lastRecord[0])
+	if err != nil {
+		return 0, err
+	}
+
+	return lastID + 1, nil
 }
 
 func init() {
 	rootCmd.AddCommand(addCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// addCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// addCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
