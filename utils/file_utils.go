3fb33d9560fb78a797207c3995d526779546adcd
Chad <chad1020@gmail.com>
2024-08-22 12:02:14 -0500

    added base list without flag

--- utils/file_utils.go
+++ utils/file_utils.go
@@ -5,7 +5,15 @@ import (
 	"syscall"
 )
 
-// LoadFile opens a file, creates it if it doesn't exist, and locks it exclusively
+// LoadFile opens a file, creates it if it doesn't exist, and locks it exclusively.
+// It returns a pointer to the file and an error if the operation fails.
+//
+// Parameters:
+// - filepath: The path to the file to open or create.
+//
+// Returns:
+// - *os.File: The file pointer to the opened file.
+// - error: An error if the operation fails, otherwise nil.
 func LoadFile(filepath string) (*os.File, error) {
 	f, err := os.OpenFile(filepath, os.O_RDWR|os.O_CREATE, os.ModePerm)
 	if err != nil {
@@ -21,7 +29,8 @@ func LoadFile(filepath string) (*os.File, error) {
 	return f, nil
 }
 
-// CloseFile unlocks and closes the file
+// closeFile unlocks and closes the file.
+// This function ensures the file lock is released before closing the file.
 func CloseFile(f *os.File) error {
 	if err := syscall.Flock(int(f.Fd()), syscall.LOCK_UN); err != nil {
 		return err
